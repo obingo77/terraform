62a5302b634bc24e93c8ee4cc8cf1158f6e57be5
obingo77 <56453225+obingo77@users.noreply.github.com>
2021-07-01 02:26:12 +0000

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -11,3 +11,14 @@ resource "aws_vpc" "shan_vpc" {
     Name = "shan_vpc-${random_pet.name.id}"
   }
 }
+
+resource "aws_subnet" "shan_public_subnet" {
+  count = length(var.public_cdrs)
+  vpc_id = aws_vpc.shan_vpc.id
+  cidr_block = var.public_cdrs[count.index]
+  map_public_ip_on_launch = true
+  availabity_zone =[count.index]
+  
+  tags ={
+    Name = "shan_public_$(count.index + 1)"
+  }
