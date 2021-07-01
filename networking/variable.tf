d2d165e7ec16635a75f9f079c4f9f026f466fb2a
obingo77 <56453225+obingo77@users.noreply.github.com>
2021-07-01 02:59:43 +0000

    Create variable.tf

---
+++ networking/variable.tf
@@ -0,0 +1,19 @@
+variable "vpc_cdr" {
+  type = string
+}
+
+variable "public_cdr" {
+  type = list
+}
+
+variable "private_cdr" {
+  type list
+}
+
+variable "public_sn_count" {
+  type =number
+}
+
+variable "public_sn_count" {
+  type = number
+}
