6288e90a8585a3e6117d7b3366fd6aa0afd35c44
obingo77 <56453225+obingo77@users.noreply.github.com>
2021-07-01 03:25:56 +0000

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -1,6 +1,15 @@
 #-----networking-----
 
+data "aws_availability_zones" "available" {}
+
 resource "random_pet" "name" {}
+
+resource "random_shuffle" "az_list" {
+  input = aws_availability_zones.available.names
+  result_count = var.max_subnets
+}
+
+
 
 resource "aws_vpc" "shan_vpc" {
   cidr_block = var.vpc_cdr
@@ -17,7 +26,7 @@ resource "aws_subnet" "shan_public_subnet" {
   vpc_id = aws_vpc.shan_vpc.id
   cidr_block = var.public_cdrs[count.index]
   map_public_ip_on_launch = true
-  availabity_zone =[count.index]
+  availabity_zone = data.aws_availability_zones.available[count.index]
   
   tags ={
     Name = "shan_public_$(count.index + 1)"
@@ -28,7 +37,7 @@ resource "aws_subnet" "shan_public_subnet" {
   vpc_id = aws_vpc.shan_vpc.id
   cidr_block = var.private_cdrs[count.index]
   map_public_ip_on_launch false
-  availabity_zone =[count.index]
+  availabity_zone = data.aws_availability_zones.available[count.index]
   
   tags ={
     Name = "shan_private_$(count.index + 1)"
