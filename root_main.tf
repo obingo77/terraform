95cbc58d2aef057dd95fb65315938d2771d7f3c2
obingo77 <56453225+obingo77@users.noreply.github.com>
2021-07-01 03:30:05 +0000

    Update root_main.tf

--- root_main.tf
+++ root_main.tf
@@ -4,7 +4,8 @@ module "networking" {
   source = "./networking"
   vpc_cidr = "10.123.0.0/16"
   public_sn_count = 2
-  private_sn_count = 3
+  private_sn_count = 5
+  max_subnets =20
   public_cdrs = [for i in range(2 ,255, 2) : cidr_subnet("10.123.0.0/16",8, i)]
   private_cdrs = [for i in range(1 ,255, 2) : cidr_subnet("10.123.0.0/16",8, i)]
 }
